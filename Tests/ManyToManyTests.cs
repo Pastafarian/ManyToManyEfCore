a14376284c6a92db0f3d81d225e73d0dc19907d7
Steve <stephen.adam@gmail.com>
2020-06-30 15:54:05 +0100

    Tests pass.

--- Tests/ManyToManyTests.cs
+++ Tests/ManyToManyTests.cs
@@ -1,5 +1,7 @@
+using System.Linq;
 using EfCore;
 using EfCore.Entities;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace Tests
@@ -17,18 +19,47 @@ namespace Tests
 
 			context.Claims.Add(idClaim);
 			context.Claims.Add(nameClaim);
-			context.SaveChanges();
 
 			var userDave = new User {Name = "Dave"};
 			context.Users.Add(userDave);
+			
+			context.UserClaims.Add(new UserClaim { Claim = idClaim, User = userDave});
+			context.UserClaims.Add(new UserClaim { Claim = nameClaim, User = userDave});
+
 			context.SaveChanges();
 
-			context.UserClaims.Add(new UserClaim { Claim = idClaim, ClaimId = idClaim.Id, User = userDave, UserId = userDave.Id});
-			context.UserClaims.Add(new UserClaim  { Claim = nameClaim, ClaimId = nameClaim.Id, User = userDave, UserId = userDave.Id});
+
+			idClaim = new Claim { Name = "Id", Value = "2" };
+			nameClaim = new Claim { Name = "Name", Value = "Steve" };
+
+			context.Claims.Add(idClaim);
+			context.Claims.Add(nameClaim);
+
+			var userSteve = new User { Name = "Steve" };
+			context.Users.Add(userSteve);
+
+			context.UserClaims.Add(new UserClaim { Claim = idClaim, User = userSteve });
+			context.UserClaims.Add(new UserClaim { Claim = nameClaim, User = userSteve });
 
 			context.SaveChanges();
 
-			var foo = "";
+			var dave = context.Users
+				.Include(x => x.UserClaims)
+				.Single(x => x.Id == userDave.Id);
+
+			var steve = context.Users
+				.Include(x => x.UserClaims)
+				.Single(x => x.Id == userSteve.Id);
+
+			Assert.True(dave.UserClaims.Count == 2);
+			Assert.Contains(dave.UserClaims, x => x.Claim.Name == "Name" && x.Claim.Value == "Dave");
+			Assert.Contains(dave.UserClaims, x => x.Claim.Name == "Id" && x.Claim.Value == "1");
+
+
+			Assert.True(steve.UserClaims.Count == 2);
+			Assert.Contains(steve.UserClaims, x => x.Claim.Name == "Name" && x.Claim.Value == "Steve");
+			Assert.Contains(steve.UserClaims, x => x.Claim.Name == "Id" && x.Claim.Value == "2");
+
 		}
 
 
